9a55078b2db272e4c2471f12e15da4bae4808789
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-03-08 19:40:49 +0500

    update index file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { LangProvider } from './context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <LangProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </LangProvider>
+  </HashRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
